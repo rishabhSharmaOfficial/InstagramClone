c6039d0899fad370b10bf078744f8f1b8bc7a355
Rishabh Sharma <risharma654@gmail.com>
2020-06-12 07:03:15 +0530

    added beginner layout

---
+++ app/src/main/java/com/example/instagramclone/PostActivity.java
@@ -0,0 +1,29 @@
+package com.example.instagramclone;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.instagramclone.Fragments.HomeFragment;
+import com.example.instagramclone.Fragments.NotificationFragment;
+import com.example.instagramclone.Fragments.ProfileFragment;
+import com.example.instagramclone.Fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class PostActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private Fragment selectorFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+
+
+    }
+}
